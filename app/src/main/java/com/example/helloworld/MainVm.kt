01d8b1a7e2e54d9ba181aa546ecf9f363ed12d53
boggle <1105059963@qq.com>
2023-09-18 10:26:36 +0800

    ng.rv

--- app/src/main/java/com/example/helloworld/MainVm.kt
+++ app/src/main/java/com/example/helloworld/MainVm.kt
@@ -8,8 +8,17 @@ class MainVm : ViewModel() {
 
     fun getAllData(): MutableLiveData<MutableList<ContentEntity>>{
         var temp = mutableListOf<ContentEntity>()
-        temp.add(ContentEntity("One"))
-        temp.add(ContentEntity("Two"))
+        temp.add(ContentEntity("drawColor"))
+        temp.add(ContentEntity("drawCircle"))
+        temp.add(ContentEntity("drawRect"))
+        temp.add(ContentEntity("drawPoint"))
+        temp.add(ContentEntity("drawOval"))
+        temp.add(ContentEntity("drawLine"))
+        temp.add(ContentEntity("drawRoundRect"))
+        temp.add(ContentEntity("drawArc"))
+        temp.add(ContentEntity("drawPath"))
+        temp.add(ContentEntity("直方图"))
+        temp.add(ContentEntity("饼图"))
         data = MutableLiveData(mutableListOf())
         data.value = temp
         return data
--- app/src/main/java/com/example/helloworld/vm/ItemViewModel.kt
+++ app/src/main/java/com/example/helloworld/vm/ItemViewModel.kt
@@ -6,9 +6,7 @@ import androidx.lifecycle.ViewModel
 import com.example.helloworld.ContentEntity
 
 class ItemViewModel : ViewModel() {
-
     fun onItemClick(view: View, item: Any) {
         Toast.makeText(view.context, (item as ContentEntity).content, Toast.LENGTH_SHORT).show()
     }
-
 }
